e5a0584e9a1aa5c2856f330249b393eea67c2610
MiguelBucio7 <miguelcruzb7@gmail.com>
2021-08-10 10:12:24 -0500

    home euc site

--- src/app/views/sitioEUC/sitio-EUC.routing.mudule.ts
+++ src/app/views/sitioEUC/sitio-EUC.routing.mudule.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ControlTasasComponent } from './control-tasas/control-tasas.component';
 import { ReportesEUCComponent } from './reportes-euc/reportes-euc.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-    { path: 'control-tasas', component: ControlTasasComponent },
+    { path: 'control-tasas', component: HomeComponent },
     { path: 'reportes-EUC', component: ReportesEUCComponent },
+    { path: '', component: HomeComponent, },
+    { path: '**', redirectTo: '/' }
 ];
 
 @NgModule({
