a843f93394d477eb48c85e0fc5c9ec76446fb870
Gerardo Alderete Flores <ge_call@hotmail.com>
2021-08-11 19:51:19 -0500

    Interfaz de parametros

--- src/app/views/perfilador/config/catalogos/parametros/parametros.component.ts
+++ src/app/views/perfilador/config/catalogos/parametros/parametros.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { NotificacionesService } from '../../../../../services/notificaciones.service';
 
 @Component({
   selector: 'app-parametros',
   templateUrl: './parametros.component.html',
-  styleUrls: ['./parametros.component.scss']
+  styleUrls: ['./parametros.component.scss', '../catalogos.component.scss']
 })
 export class ParametrosComponent implements OnInit {
 
-  constructor() { }
+  constructor(private notificacionesService: NotificacionesService) { }
 
   ngOnInit(): void {
+  }
+
+  save() {
+    this.notificacionesService.lanzarNotificacion('Parametro actualizado', '', 'success');
   }
 
 }
--- src/app/views/perfilador/config/catalogos/pdf-especial/pdf-especial.component.ts
+++ src/app/views/perfilador/config/catalogos/pdf-especial/pdf-especial.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-pdf-especial',
   templateUrl: './pdf-especial.component.html',
-  styleUrls: ['./pdf-especial.component.scss']
+  styleUrls: ['./pdf-especial.component.scss', '../catalogos.component.scss']
 })
 export class PdfEspecialComponent implements OnInit {
   iconCarga:string = 'assets/images/Img/load.PNG';
