65515be440aea1e5d281f2d9a4f474f977236107
Gerardo Alderete Flores <ge_call@hotmail.com>
2021-08-11 20:18:12 -0500

    interfaz de consultas

--- src/app/views/perfilador/config/catalogos/consultas/consultas.component.ts
+++ src/app/views/perfilador/config/catalogos/consultas/consultas.component.ts
@@ -1,28 +1,49 @@
 import { Component, OnInit } from '@angular/core';
 
+enum catalogs {
+  CatCampanias = 'Catalogo de Campañas',
+  CatOfertas = 'Catálogo de Ofertas',
+  CaTasas = 'Catálogo de tasas',
+  CatGuias = 'Catálogo de Guía Informativa',
+  CatPDFEspecial = 'Catálogo de PDF especial',
+  CatFolios = 'Catálogo de folios',
+  CatSucursales = 'Catálogo de sucursales',
+  CatParametros = 'Carga de valor de parámetros',
+}
+
 @Component({
   selector: 'app-consultas',
   templateUrl: './consultas.component.html',
-  styleUrls: ['./consultas.component.scss']
+  styleUrls: ['./consultas.component.scss', '../catalogos.component.scss']
 })
 export class ConsultasComponent implements OnInit {
+  files: {key:string,label}[] = []
 
-  constructor() { }
+  constructor() { 
+    for (let motif in catalogs) {
+      this.files.push({
+        key: motif,
+        label: catalogs[motif]
+      })
+        console.log(catalogs[motif]);
+    }
+  console.log(this.files);
+  }
 
   ngOnInit(): void {
   }
 
   downloadPdf(base64String, fileName) {
-    const source = `data:application/pdf;base64,${base64String}`;
+    const source = `${base64String}`;
     const link = document.createElement("a");
     link.href = source;
-    link.download = `${fileName}.pdf`
+    link.download = `${fileName}.txt`
     link.click();
   }
 
-  onClickDownloadPdf(){
+  onClickDownloadPdf(nameFile: string){
     let base64String = "data:text/plain;base64,Y2FtcGHDsWEtIGRlc2NhcmdhIGFyY2hpdm8gdHh0DQpvZmVydGEgLSBkZXNjYXJnYSBhcmNoaXZvIHR4dA0KdGFzYXMgLSBDb25maWd1cmFjacOzbi0+IENhdGFsb2dvLT7igJ1UYXNhcyBkZWwgY29tcGFyYXRpdm/igJ0tPiANCg0KDQo=";
-    this.downloadPdf(base64String,"export");
+    this.downloadPdf(base64String, nameFile);
   }
 
 }
