e682b733f2a5fbc32c2c8e502b78b7d049663ce2
Gerardo Alderete Flores <ge_call@hotmail.com>
2021-08-23 17:05:49 -0500

    interfaz carga de catalogo de sucursal

--- src/app/views/perfilador/config/catalogos/sucursales/sucursales.component.ts
+++ src/app/views/perfilador/config/catalogos/sucursales/sucursales.component.ts
@@ -5,41 +5,84 @@ import {MatTableDataSource} from '@angular/material/table';
 import Swal from 'sweetalert2';
 import { NotificacionesService } from '../../../../../services/notificaciones.service';
 
-interface IPDFEspecial {
-  pdfEspecialOfertaId?: string,
-  pdfEspecialNombreArchivo?: IPdf,
-  pdfEspecialUrl?: string,
-  pdfEspecialTipo?: boolean,
+interface ISucursal {
+  sirhSucursalId?: number,
+  nombreSucursal: string,
+  distritoId: number,
+  distritoNombre: string,
+  divisionId: number,
+  divisionNombre: string,
+  tipoDispositivo: string,
+  ejecutivoEAV: number,
+  ejecutivoPriority: number,
+  ejecutivoTotal: number,
+  isClosed: boolean,
 }
 
-interface IPdf {
-  name: string,
-  base64: string,
-}
-
- 
 const bodyTemplateGuide = ` 
 <form>
   <div class="form-group row flex-column">
-    <label for="pdfEspecialNombreArchivo" class="col col-form-label">Nombre del archivo</label>
+    <label for="nombreSucursal" class="col col-form-label">Nombre de la Sucursal</label>
     <div >
-      <input type="text"  class="form-control-plaintext form-control-sm" id="pdfEspecialNombreArchivo">
-    </div>
-  </div>
-  <div class="form-group row flex-column">
-    <label for="pdfEspecialNombreArchivo" class="col col-form-label">Archivo</label>
-    <div class="col">
-      <input type="file"  id="pdfEspecialFile" accept="pdf/*" style="display: none;">
-      <label class="label-file" for="pdfEspecialFile">Selecciona un Archivo...</label>
+      <input type="text"  class="form-control-plaintext form-control-sm" id="nombreSucursal">
     </div>
   </div>
   <div class="row">
     <div class="form-group ml-2 col">
-      <label for="pdfEspecialTipo">Tipo de PDF Epecial.</label>
-      <select class="form-control-plaintext form-control-sm" style="width: 163px; margin: auto;" id="pdfEspecialTipo">
-        <option value="true">Foleado</option>
-        <option value="false">No Foleado</option>
+      <label for="distritoNombre">Distrito</label>
+      <select class="form-control-plaintext form-control-sm" style="width: 163px; margin: auto;" id="distritoNombre">
+        <option value="1">Distrito 1</option>
+        <option value="2">Distrito 2</option>
       </select>
+    </div>
+  </div>
+  <div class="row">
+    <div class="form-group ml-2 col">
+      <label for="divisionNombre">División</label>
+      <select class="form-control-plaintext form-control-sm" style="width: 163px; margin: auto;" id="divisionNombre">
+        <option value="1">División 1</option>
+        <option value="2">División 2</option>
+      </select>
+    </div>
+  </div>
+  <div class="row">
+    <div class="form-group ml-2 col">
+      <label for="tipoDispositivo">Tipo de dispositivo</label>
+      <select class="form-control-plaintext form-control-sm" style="width: 163px; margin: auto;" id="tipoDispositivo">
+        <option value="1">Dispositivo 1</option>
+        <option value="2">Dispositivo 2</option>
+      </select>
+    </div>
+  </div>
+  <div class="form-group row flex-column">
+    <label for="ejecutivoEAV" class="col col-form-label">Número de ejecutivos no priority asignados</label>
+    <div >
+      <input type="number" class="form-control-plaintext form-control-sm" id="ejecutivoEAV">
+    </div>
+  </div>
+  <div class="form-group row flex-column">
+    <label for="ejecutivoPriority" class="col col-form-label">Número total de Ejecutivos Prioririty asignados</label>
+    <div >
+      <input type="number" class="form-control-plaintext form-control-sm" id="ejecutivoPriority">
+    </div>
+  </div>
+  <div class="form-group row flex-column">
+    <label for="ejecutivoTotal" class="col col-form-label">Número de ejecutivo total de la sucursal</label>
+    <div >
+      <input type="number" class="form-control-plaintext form-control-sm" id="ejecutivoTotal">
+    </div>
+  </div>
+  <div class="row justify-content-center flex-column">
+    <label>¿Esta cerrado?</label>
+    <div>
+      <div class="form-check form-check-inline">
+        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="Is_Closed1" value="true">
+        <label class="form-check-label" for="inlineRadio1">Si</label>
+      </div>
+      <div class="form-check form-check-inline">
+        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="Is_Closed2" value="false">
+        <label class="form-check-label" for="inlineRadio2">No</label>
+      </div>
     </div>
   </div>
 </form>  
@@ -54,8 +97,20 @@ import { base64Pdf } from '../../../../../../assets/data/pdf-base-64';
   styleUrls: ['./sucursales.component.scss', '../catalogos.component.scss', '../ejecutivos-sucursal/ejecutivos-sucursal.component.scss']
 })
 export class SucursalesComponent implements OnInit {
-  displayedColumns: string[] = ['pdfEspecialOfertaId', 'pdfEspecialNombreArchivo', 'pdfEspecialUrl', 'pdfEspecialTipo' , 'edit'];
-  dataSource: MatTableDataSource<IPDFEspecial>;
+  displayedColumns: string[] = [
+    'sirhSucursalId',
+    'nombreSucursal', 
+    'distritoId',
+    'distritoNombre' ,
+    'divisionId' ,
+    'divisionNombre' ,
+    'tipoDispositivo' ,
+    'ejecutivoEAV' ,
+    'ejecutivoPriority' , 
+    'ejecutivoTotal' ,
+    'isClosed' ,
+    'edit'];
+  dataSource: MatTableDataSource<ISucursal>;
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
@@ -64,28 +119,24 @@ export class SucursalesComponent implements OnInit {
 
   constructor(private notificacionesService: NotificacionesService) {
     // Create 100 users
-    const pdfsEspeciales: IPDFEspecial[] = [
+    const sucursales: ISucursal[] = [
       {
-        pdfEspecialOfertaId: '1',
-        pdfEspecialNombreArchivo: {
-          name: 'nombre archivo 1',
-          base64: base64Pdf
-        } ,
-        pdfEspecialTipo: true,
-        pdfEspecialUrl: 'https://www.alberguemontfalco.com/fotosbd/120520140951280826.pdf'
-      },
-      {
-        pdfEspecialOfertaId: '2',
-        pdfEspecialNombreArchivo: {
-          name: 'nombre archivo 2',
-          base64: base64Pdf
-        } ,        pdfEspecialTipo: false,
-        pdfEspecialUrl: 'https://www.alberguemontfalco.com/fotosbd/120520140951280826.pdf'
+        sirhSucursalId: 1,
+        nombreSucursal: 'Sucursal1',
+        distritoId: 2,
+        distritoNombre: 'Distrito2',
+        divisionId: 3,
+        divisionNombre: 'Division3',
+        tipoDispositivo: 'Dispositivo 1',
+        ejecutivoEAV: 5,
+        ejecutivoPriority: 3,
+        ejecutivoTotal: 8,
+        isClosed: false,
       },
     ]  
 
     // Assign the data to the data source for the table to render
-    this.dataSource = new MatTableDataSource(pdfsEspeciales);
+    this.dataSource = new MatTableDataSource(sucursales);
   }
 
   ngOnInit(): void { 
@@ -97,39 +148,81 @@ export class SucursalesComponent implements OnInit {
   }
 
   openModalAdd() {
-    this.openModal('Agregar', 'Guardar', 'Regresar','Ejecutivo agregado exitosamente','Error al agregar ejecutivo', this.bodyTemplateEjecutivo);
+    this.openModal('Agregar', 'Guardar', 'Regresar','Sucursal agregada exitosamente','Error al agregar sucursal', this.bodyTemplateEjecutivo);
   }
 
-  openModalEdit( pdfEspecial: IPDFEspecial) {
-    console.log(pdfEspecial);
+  openModalEdit( sucursal: ISucursal) {
+    console.log(sucursal);
     let body = `
     <form>
-        <div class="form-group row flex-column">
-          <label for="pdfEspecialNombreArchivo" class="col col-form-label">Nombre del archivo</label>
-          <div >
-            <input type="text"  class="form-control-plaintext form-control-sm" id="pdfEspecialNombreArchivo" value="${pdfEspecial.pdfEspecialNombreArchivo.name}">
-          </div>
-        </div>
-        <div class="form-group row flex-column">
-          <label for="pdfEspecialNombreArchivo" class="col col-form-label">Archivo</label>
-          <div class="col">
-            <input type="file"  id="pdfEspecialFile" accept="pdf/*" style="display: none;">
-            <label class="label-file" for="pdfEspecialFile">Selecciona un Archivo...</label>
-          </div>
-        </div>
-        <div class="row">
-          <div class="form-group ml-2 col">
-            <label for="pdfEspecialTipo">Tipo de PDF Epecial.</label>
-            <select class="form-control-plaintext form-control-sm" style="width: 163px; margin: auto;" id="pdfEspecialTipo">
-              <option selected="${pdfEspecial.pdfEspecialTipo === false ? true : false}" value="true">Foleado</option>
-              <option selected="${pdfEspecial.pdfEspecialTipo === true ? true : false}" value="false">No Foleado</option>
-            </select>
-          </div>
-        </div>
-      </form> 
+  <div class="form-group row flex-column">
+    <label for="nombreSucursal" class="col col-form-label">Nombre de la Sucursal</label>
+    <div >
+      <input type="text"  class="form-control-plaintext form-control-sm" id="nombreSucursal" value="${sucursal.nombreSucursal}">
+    </div>
+  </div>
+  <div class="row">
+    <div class="form-group ml-2 col">
+      <label for="distritoNombre">Distrito</label>
+      <select class="form-control-plaintext form-control-sm" style="width: 163px; margin: auto;" id="distritoNombre">
+        <option value="1">Distrito 1</option>
+        <option value="2">Distrito 2</option>
+      </select>
+    </div>
+  </div>
+  <div class="row">
+    <div class="form-group ml-2 col">
+      <label for="divisionNombre">División</label>
+      <select class="form-control-plaintext form-control-sm" style="width: 163px; margin: auto;" id="divisionNombre">
+        <option value="1">División 1</option>
+        <option value="2">División 2</option>
+      </select>
+    </div>
+  </div>
+  <div class="row">
+    <div class="form-group ml-2 col">
+      <label for="tipoDispositivo">Tipo de dispositivo</label>
+      <select class="form-control-plaintext form-control-sm" style="width: 163px; margin: auto;" id="tipoDispositivo">
+        <option value="1">Dispositivo 1</option>
+        <option value="2">Dispositivo 2</option>
+      </select>
+    </div>
+  </div>
+  <div class="form-group row flex-column">
+    <label for="ejecutivoEAV" class="col col-form-label">Número de ejecutivos no priority asignados</label>
+    <div >
+      <input type="number" class="form-control-plaintext form-control-sm" id="ejecutivoEAV" value="${sucursal.ejecutivoEAV}">
+    </div>
+  </div>
+  <div class="form-group row flex-column">
+    <label for="ejecutivoPriority" class="col col-form-label">Número total de Ejecutivos Prioririty asignados</label>
+    <div >
+      <input type="number" class="form-control-plaintext form-control-sm" id="ejecutivoPriority" value="${sucursal.ejecutivoPriority}">
+    </div>
+  </div>
+  <div class="form-group row flex-column">
+    <label for="ejecutivoTotal" class="col col-form-label">Número de ejecutivo total de la sucursal</label>
+    <div >
+      <input type="number" class="form-control-plaintext form-control-sm" id="ejecutivoTotal" value="${sucursal.ejecutivoTotal}">
+    </div>
+  </div>
+  <div class="row justify-content-center flex-column">
+    <label>¿Esta cerrado?</label>
+    <div>
+      <div class="form-check form-check-inline">
+        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="Is_Closed1" value="true" checked="true">
+        <label class="form-check-label" for="inlineRadio1">Si</label>
+      </div>
+      <div class="form-check form-check-inline">
+        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="Is_Closed2" value="false" checked="false">
+        <label class="form-check-label" for="inlineRadio2">No</label>
+      </div>
+    </div>
+  </div>
+</form>  
     `;
 
-    this.openModal('EDICIÓN DE USUARIO', 'GUARDAR', 'CANCELAR','Usuario editado exitosamente','Error al editar la usuario', body);
+    this.openModal('EDICIÓN DE SUCURSAL', 'GUARDAR', 'CANCELAR','Sucursal editada exitosamente','Error al editar la sucursal', body);
   }
 
   openModal(title: string, btnOk: string, btnCancel: string, successMessage: string, errorMessage: string, body: string ) {
